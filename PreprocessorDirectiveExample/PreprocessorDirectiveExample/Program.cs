e056d0f55bc55def9cdb07e58cc3af7f2304f801
siddhartha1988 <siddeee@gmail.com>
2024-04-19 16:46:33 +0530

    Commit to explain Preprocessor Directives

---
+++ PreprocessorDirectiveExample/PreprocessorDirectiveExample/Program.cs
@@ -0,0 +1,19 @@
+﻿#define ValidCase1
+using System;
+namespace PreprocessorDirective
+{
+    public class Preprocessor
+    {
+        public static void Main()
+        {
+#if ValidCase1
+            Console.WriteLine("ValidCase1");
+#elif ValidCase2
+            Console.WriteLine("ValidCase2");
+#else
+            Console..WriteLine("InvalidCase");
+#endif
+
+        }
+    }
+}
